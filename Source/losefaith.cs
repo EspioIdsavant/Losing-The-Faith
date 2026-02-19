669023d5688f3c4fd4228cc118d6fc51d89486fb
EspioIdsavant <espio@outlook.com>
2026-02-19 08:31:57 -0600

    Initial upload

---
+++ Source/losefaith.cs
@@ -0,0 +1,22 @@
+using RimWorld;
+using Verse;
+
+namespace losefaith
+{
+    [StaticConstructorOnStartup]
+    public static class LoseTheFaith
+    {
+        static LoseTheFaith()
+        {
+            ConversionTuning.CertaintyPerDayByMoodCurve.SetPoints(new SimpleCurve
+            {
+                new CurvePoint(0.0f, -0.02f),
+                new CurvePoint(0.2f, -0.01f),
+                new CurvePoint(0.4f, 0.00f),
+                new CurvePoint(0.6f, 0.01f),
+                new CurvePoint(0.8f, 0.02f),
+                new CurvePoint(0.9f, 0.03f)
+            });
+        }
+    }
+ }
